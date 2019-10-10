d6a7124260a1360f8ddc35875fd23e489f984331
Jacob Bankston <jacob.bankston.smile@gmail.com>
2019-10-09 21:10:29 -0500

    updating the function

--- tes.js
+++ tes.js
@@ -3,5 +3,6 @@ const Gpio = require("onoff").Gpio;
 const gpioswitch = new Gpio(3, "out"); // creating a new output relationship with pin 2
 
 gpioswitch.writeSync(1);
-
-setTimeout(gpioswitch.writeSync(0), 10000);
+setTimeout(function() {
+  gpioswitch.writeSync(0);
+}, 10000);
